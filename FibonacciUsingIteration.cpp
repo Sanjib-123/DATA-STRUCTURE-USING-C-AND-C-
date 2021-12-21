e383136918ce6507b3ecb123f97c7ecebcf9a694
Sanjib-123 <84591644+Sanjib-123@users.noreply.github.com>
2021-12-21 11:03:54 +0530

    Update FibonacciUsingIteration.cpp

--- FibonacciUsingIteration.cpp
+++ FibonacciUsingIteration.cpp
@@ -12,10 +12,42 @@ int fib(int n){
     }
     return s;
 }
+int rfib(int n){
+    if(n <= 1)
+        return n;
+        return rfib(n - 2) + rfib(n - 1);
+    
+}
+int F[10];
+int mfib(int n)
+{
+    if(n <= 1)
+    {
+        F[n] = n;
+        return n;
+    }else
+    {
+        if(F[n - 2] == -1)
+        {
+            F[n - 2] = mfib(n - 2);
+        }
+        if(F[n - 1] == -1)
+        {
+            F[n - 1] = mfib(n - 1);
+            F[n] = F[n - 2] + F[n - 1];
+           
+        }
+         return F[n - 2] + F[n - 1];
+    }
+    
+}
 
 int main()
 {
-   cout<<fib(6)<<endl;
+     for (int i = 0; i < 10; i++) {
+        F[i] = -1;
+    }
+   cout<<mfib(6)<<endl;
 
     return 0;
 }
